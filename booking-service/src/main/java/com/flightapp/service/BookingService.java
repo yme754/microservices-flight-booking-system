c2824135991bba8122451d107935fb8333c7fa74
Yaswitha Sakhamuri <yaswitha@example.com>
2025-11-30 11:37:04 +0530

    added booking service

---
+++ booking-service/src/main/java/com/flightapp/service/BookingService.java
@@ -0,0 +1,18 @@
+package com.flightapp.service;
+
+import java.util.List;
+
+import com.flightapp.entity.Booking;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BookingService {
+	Mono<Booking> createBooking(Booking booking);
+    Mono<Booking> getBookingByPnr(String pnr);
+    Flux<Booking> getAllBookings();
+    Mono<Void> deleteBooking(String id);
+    Mono<Booking> updateSeatNumbers(String bookingId, List<String> seatNumbers);
+    Mono<Booking> updatePassengerIds(String bookingId, List<String> passengerIds);
+    Mono<Booking> updateTotalAmount(String bookingId, float amount);
+}
