d43954d3998797bb0794fbb41206bf551f5ba1d5
Yaswitha Sakhamuri <yaswitha@example.com>
2025-12-04 16:48:43 +0530

    updated post methods

--- booking-service/src/main/java/com/flightapp/controller/BookingController.java
+++ booking-service/src/main/java/com/flightapp/controller/BookingController.java
@@ -1,13 +1,16 @@
 package com.flightapp.controller;
 
+import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.flightapp.dto.BookingDTO;
@@ -27,15 +30,15 @@ public class BookingController {
     }
 
     @PostMapping
-    public Mono<BookingDTO> createBooking(@RequestBody BookingDTO bookingDTO) {
-        return bookingService.createBooking(toEntity(bookingDTO))
-                .map(this::toDto);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Map<String, String>> createBooking(@RequestBody BookingDTO bookingDTO) {
+        return bookingService.createBooking(toEntity(bookingDTO)).map(booking -> Map.of("id", booking.getId()));
     }
-
+    
     @PostMapping("/book")
-    public Mono<BookingDTO> bookFlight(@RequestBody BookingDTO bookingDTO) {
-        return bookingService.bookFlight(toEntity(bookingDTO))
-                .map(this::toDto);
+    @ResponseStatus(HttpStatus.CREATED) // Sets status to 201
+    public Mono<Map<String, String>> bookFlight(@RequestBody BookingDTO bookingDTO) {
+        return bookingService.bookFlight(toEntity(bookingDTO)).map(booking -> Map.of("id", booking.getId()));
     }
 
     @GetMapping("/{pnr}")
